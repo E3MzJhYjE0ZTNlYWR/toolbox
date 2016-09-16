6d4c6abff02052d5b19ad463792656b08f89f047
Andrey Chausenko <Andrey.Chausenko@team.telstra.com>
2016-09-16 18:46:44 +1000

    Releasing version 1.0.14

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cd /home &&
 openssl enc -d -aes-256-cbc -in package.zip.enc -out package.zip -pass pass:$UNLOCK_KEY &&
-unzip package.zip && 
+unzip -o package.zip && 
 mv /home/package/_s3cfg /root/.s3cfg && 
 mv /home/package/lftp.conf /etc/lftp.conf
 
